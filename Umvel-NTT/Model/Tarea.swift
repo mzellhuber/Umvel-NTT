c361d647e7a14dfb9cd252943f159fbafd5bb2b3
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2023-09-22 15:06:06 -0600

    Tarea Model

---
+++ Umvel-NTT/Model/Tarea.swift
@@ -0,0 +1,14 @@
+//
+//  Tarea.swift
+//  Umvel-NTT
+//
+//  Created by Melissa Zellhuber on 22/09/23.
+//
+
+import Foundation
+
+struct Tarea: Identifiable {
+    var id = UUID()
+    var nombre: String
+    var estaCompleta: Bool
+}
