493a85d4b9b48cb0365e8772407533815d00cc4b
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2023-09-22 15:04:32 -0600

    Initial Commit

---
+++ Umvel-NTT/Umvel_NTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Umvel_NTTApp.swift
+//  Umvel-NTT
+//
+//  Created by Melissa Zellhuber on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Umvel_NTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
